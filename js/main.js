19a1af66bf879161277636f23cd7082316724b43
Leslie DeShaw <l_deshaw@fanshaweonline.ca>
2022-01-29 09:55:10 -0500

    finishingtouches

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 //this is a self - invoking anonymous function
 //its called the module pattern
+ 
+ 
 (() => {
-  console.log('this is some text');
   //go into the document and make a reference to an element that you want to work with
-  let theHeader = document.querySelector('#main-header');
+  let theHeading = document.querySelector(".main-heading");
+      theIcons = document.querySelector(".icons");
+ // debugger; //this is like a breakpoint/ pause point in your code
+ 
+ 
 //create a function that runs when theheader element is clicked
   function logElement() {
-    console.log('clicked on an element!');
+    console.log('clicked on an element!', this.id);
   }
-  
+ 
   //listen for a user event, and then do something
   // the "do something" is the function we run when the element is clicked
-  theHeader.addEventListener("click", logElement);
+  theHeading.addEventListener("click", logElement);
+  theIcons.forEach(addEventListener("click", logElement));
 })();
